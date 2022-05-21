d780c7a96be26a7f58028390ce02fed24fa8d6b9
SJG <shinjg97@gmail.com>
2022-05-21 20:33:55 +0900

    랭킹 시스템 구현중

--- app/src/main/java/org/techtown/booktree_firebase/RankingActivity.java
+++ app/src/main/java/org/techtown/booktree_firebase/RankingActivity.java
@@ -1,6 +1,14 @@
 package org.techtown.booktree_firebase;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RankingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
 }
